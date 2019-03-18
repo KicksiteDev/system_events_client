02289612a85991ea633e4cce927dc404034af89d
Lee DeBoom <leedeboom@gmail.com>
2019-03-18 17:23:51 -0500

    DEV-2253 Provide bearer authentication in rails 4 kicksite-crud (#2)

---
+++ lib/system_events_client/app/helpers/bearer_auth_connection.rb
@@ -0,0 +1,18 @@
+# Override of ActiveResource Connection class to provider bearer authentication
+class BearerAuthConnection < ActiveResource::Connection
+  attr_accessor :bearer_token
+
+  private
+
+  def authorization_header(_http_method, _uri)
+    if bearer_token && auth_type == :bearer
+      { 'Authorization' => "Bearer #{bearer_token}" }
+    else
+      {}
+    end
+  end
+
+  def legitimize_auth_type(auth_type)
+    auth_type.nil? ? :basic : :bearer
+  end
+end
