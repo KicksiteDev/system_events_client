c76eec121ad0d0874c73ecbb0dfc5dce07160518
Lee DeBoom <lee@kicksite.net>
2019-09-19 15:55:42 -0500

    DEV-3068

--- lib/system_events_client.rb
+++ lib/system_events_client.rb
@@ -4,16 +4,17 @@ Dir["#{File.dirname(__FILE__)}/system_events_client/**/*.rb"].each { |file| requ
 module SystemEventsClient
   raise 'Set SYSTEM_EVENTS_API_URL environment variable to utilize this gem' if ENV['SYSTEM_EVENTS_API_URL'].blank?
 
-  STUDENT_EVENT_TYPE     = 'student'.freeze
-  PROSPECT_EVENT_TYPE    = 'prospect'.freeze
-  PAYMENT_EVENT_TYPE     = 'payment'.freeze
-  MEMBERSHIP_EVENT_TYPE  = 'membership'.freeze
-  INVOICE_EVENT_TYPE     = 'invoice'.freeze
-  EVENT_EVENT_TYPE       = 'event'.freeze
-  FAMILY_EVENT_TYPE      = 'family'.freeze
-  COMMENT_EVENT_TYPE     = 'comment'.freeze
-  TASK_EVENT_TYPE        = 'task'.freeze
-  APPOINTMENT_EVENT_TYPE = 'appointment'.freeze
+  STUDENT_EVENT_TYPE            = 'student'.freeze
+  PROSPECT_EVENT_TYPE           = 'prospect'.freeze
+  PAYMENT_EVENT_TYPE            = 'payment'.freeze
+  MEMBERSHIP_EVENT_TYPE         = 'membership'.freeze
+  INVOICE_EVENT_TYPE            = 'invoice'.freeze
+  EVENT_EVENT_TYPE              = 'event'.freeze
+  FAMILY_EVENT_TYPE             = 'family'.freeze
+  COMMENT_EVENT_TYPE            = 'comment'.freeze
+  TASK_EVENT_TYPE               = 'task'.freeze
+  APPOINTMENT_EVENT_TYPE        = 'appointment'.freeze
+  BILLING_CONNECTION_EVENT_TYPE = 'billing_connection'.freeze
 
   CREATE_EVENT_ACTION   = 'create'.freeze
   UPDATE_EVENT_ACTION   = 'update'.freeze
