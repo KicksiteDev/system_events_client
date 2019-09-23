0d381dd092ebd6e31cdad04fdfb4bd0e400b3ca6
Lee DeBoom <lee@kicksite.net>
2019-09-23 12:35:24 -0500

    DEV-3068 added a quick event type literal for billing connection type events (#7)

--- lib/system_events_client.rb
+++ lib/system_events_client.rb
@@ -2,18 +2,24 @@ Dir["#{File.dirname(__FILE__)}/system_events_client/**/*.rb"].each { |file| requ
 
 # Entry to REST api model definitions
 module SystemEventsClient
-  raise 'Set SYSTEM_EVENTS_API_URL environment variable to utilize this gem' if ENV['SYSTEM_EVENTS_API_URL'].blank?
+  # Note: Anyone consuming this should mock the actual REST calls in their tests
+  unless ENV['RAILS_ENV'] == 'test'
+    if ENV['SYSTEM_EVENTS_API_URL'].blank?
+      raise 'Set SYSTEM_EVENTS_API_URL environment variable to utilize this gem'
+    end
+  end
 
-  STUDENT_EVENT_TYPE     = 'student'.freeze
-  PROSPECT_EVENT_TYPE    = 'prospect'.freeze
-  PAYMENT_EVENT_TYPE     = 'payment'.freeze
-  MEMBERSHIP_EVENT_TYPE  = 'membership'.freeze
-  INVOICE_EVENT_TYPE     = 'invoice'.freeze
-  EVENT_EVENT_TYPE       = 'event'.freeze
-  FAMILY_EVENT_TYPE      = 'family'.freeze
-  COMMENT_EVENT_TYPE     = 'comment'.freeze
-  TASK_EVENT_TYPE        = 'task'.freeze
-  APPOINTMENT_EVENT_TYPE = 'appointment'.freeze
+  STUDENT_EVENT_TYPE            = 'student'.freeze
+  PROSPECT_EVENT_TYPE           = 'prospect'.freeze
+  PAYMENT_EVENT_TYPE            = 'payment'.freeze
+  MEMBERSHIP_EVENT_TYPE         = 'membership'.freeze
+  INVOICE_EVENT_TYPE            = 'invoice'.freeze
+  EVENT_EVENT_TYPE              = 'event'.freeze
+  FAMILY_EVENT_TYPE             = 'family'.freeze
+  COMMENT_EVENT_TYPE            = 'comment'.freeze
+  TASK_EVENT_TYPE               = 'task'.freeze
+  APPOINTMENT_EVENT_TYPE        = 'appointment'.freeze
+  BILLING_CONNECTION_EVENT_TYPE = 'billing_connection'.freeze
 
   CREATE_EVENT_ACTION   = 'create'.freeze
   UPDATE_EVENT_ACTION   = 'update'.freeze
